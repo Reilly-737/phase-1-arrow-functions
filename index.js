60a247b7c6d351c48811b664880bde7ea94fec27
+ <+>
2023-07-31 17:51:47 -0600

    finish him

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (numerator, denominator) => {
+    return numerator / denominator;
+}
+const square = (number) => {
+    return number * number;
+};
+
+const add = (num1, num2) => {
+    return num1 + num2
+};
